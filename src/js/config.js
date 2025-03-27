0dbc7321a607da0ac9b0ff948dfd683d9f7b4cb9
Albahdezs <alba.hdez.serr@gmail.com>
2025-03-27 09:35:29 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5c1c90b6-aa19-47d8-a2f9-6a43e2583ff0';
+export const MODAL_CLOSE_SEC = 2.5;
